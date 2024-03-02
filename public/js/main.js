96dbb38638e250ab0ab18e2f93387757495986b5
Ranveer Singh Walia <ranveerwalia76@gmail.com>
2024-03-02 14:13:40 +0530

    i

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 function sign_page(){
     document.getElementsByClassName('sign-container')[0].style.display = 'block';
+    if(window.innerWidth < 771){
+        document.getElementsByClassName('head')[0].style.display = 'none';
+    }
 }
 document.getElementsByClassName('cross')[0].addEventListener('click', function(){
     document.getElementsByClassName('sign-container')[0].style.display = 'none';
+    if(window.innerWidth < 771){
+        document.getElementsByClassName('head')[0].style.display = 'flex';
+    }
 });
