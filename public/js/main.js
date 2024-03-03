988d8260e66e656fdd99901615db78d52da370d8
Ranveer Singh Walia <ranveerwalia76@gmail.com>
2024-03-03 17:57:30 +0530

    menu button

--- public/js/main.js
+++ public/js/main.js
@@ -10,3 +10,16 @@ document.getElementsByClassName('cross')[0].addEventListener('click', function()
         document.getElementsByClassName('head')[0].style.display = 'flex';
     }
 });
+
+var menu = document.getElementsByClassName('burger')[0];
+var nav = document.getElementsByClassName('head')[0].getElementsByTagName('ul');
+
+menu.addEventListener('click', function(){
+    if(nav[0].style.display == 'flex'){
+        nav[0].style.display = 'none';
+        nav[1].style.display = 'none';
+    }else{
+        nav[0].style.display = 'flex';
+        nav[1].style.display = 'flex';
+    } menu.classList.toggle('burger-active');
+});
